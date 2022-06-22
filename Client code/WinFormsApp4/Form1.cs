dbc34280175590ee0b28f34ef9fccb2113ed54db
dievoom-png <63131967+dievoom-png@users.noreply.github.com>
2022-06-22 21:46:56 +0200

    added Code

---
+++ Client code/WinFormsApp4/Form1.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Data;using System.ComponentModel;
+using System.Linq;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        private NetworkStream serverStream;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8488);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            serverStream = clientSocket.GetStream();
+            string mesg = textBox1.Text;
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(mesg + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, DoWorkEventArgs e)
+        {
+            //byte[] inStream = new byte[10025];
+            //serverStream.Read(inStream, 0, inStream.Length);
+            //string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            ////msg("Data from Server : " + returndata);
+            //label2.Text = ("Data :  " + returndata);
+
+            textBox1.Clear();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            serverStream = clientSocket.GetStream();
+            
+                byte[] inStream = new byte[10025];
+                serverStream.Read(inStream, 0, inStream.Length);
+
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream); // msg("Data from Server : " + returndata);
+
+            
+
+            label2.Text = ("Data :  " + returndata);
+            textBox1.Clear();
+        }
+
+    }
+}
---
+++ Server Code/ConsoleApplication/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, 8488);
+            TcpClient clientSocket;
+            int counter = 0;
+            serverSocket.Start();//start listener
+            Console.WriteLine(" >> " + "Server Started");
+            counter = 0;
+
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine(" >> " + "Client No:" + Convert.ToString(counter) + " started!");
+                handleClinet client = new handleClinet();
+                client.startClient(clientSocket, Convert.ToString(counter));
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
+        }
+    }
+
+    //Class to handle each client request separatly
+    public class handleClinet
+    {
+        private TcpClient clientSocket;
+        private string Client_nb;
+        public static List<TcpClient> tcpClientsList = new List<TcpClient>();
+
+        public void startClient(TcpClient inClientSocket, string clineNo)
+
+        {
+            tcpClientsList.Add(inClientSocket);
+            clientSocket = inClientSocket;
+            Client_nb = clineNo;
+            Thread ctThread = new Thread(doChat);//create thread
+            ctThread.Start();
+        }
+
+        public  void BroadCast(string msg, TcpClient excludeClient, string clineNo)
+        {
+            foreach (TcpClient client in tcpClientsList)
+            {
+                    StreamWriter sWriter = new StreamWriter(client.GetStream());
+                    sWriter.WriteLine(" >> " + "Client No:" + clineNo + msg);
+                    sWriter.Flush();
+            }
+        }
+
+        private void doChat()
+        {
+         ;
+            byte[] bytesFrom = new byte[10025];//empty array of by
+                                               //tes
+            string dtfromC = null;
+           
+            
+            
+
+            while (true)
+            {
+                try
+                {
+                  
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);//malet bytes from data
+                   
+                    dtfromC = System.Text.Encoding.ASCII.GetString(bytesFrom);//7aweltaha string
+                    dtfromC = dtfromC.Substring(0, dtfromC.IndexOf("$"));//fdelt a2ra lehad ma la2et dollar sign
+                    BroadCast(dtfromC, clientSocket, Client_nb);
+                   
+                    Console.WriteLine(" >> " + "From client-" + Client_nb + dtfromC);
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex.ToString());
+                }
+            }
+        }
+    }
+}
